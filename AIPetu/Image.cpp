4b0260781b35e213ff96fae887244ac8d256c16c
BraKann <80362852+BraKann@users.noreply.github.com>
2022-02-28 17:23:15 +0100

    add files
    pdf and AIP

---
+++ AIPetu/Image.cpp
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////
+/// This file is part of the Aerial Image Project (AIP).
+///
+/// Copyright (c) ...
+///
+/// The sources of AIP are distributed WITHOUT ANY WARRANTY.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <cassert>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include "Image.h"
+
+/*
+void Image::writeSVG(const std::string& filename, int pixelSize) const
+{
+  assert(pixelSize > 0);
+
+  std::ofstream file;
+  file.open(filename + ".svg");
+
+  if (!file) throw std::runtime_error("error open file (write SVG)");
+
+  file << "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+       << std::endl
+       << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\""
+       << pixelSize*width()
+       << "\" height=\""
+       << pixelSize*height()
+       << "\">"
+       << std::endl;
+
+  for (int i = 1; i <= height(); ++i)
+    for (int j = 1; j <= width(); ++j)
+      file << "<rect width=\""
+           << pixelSize
+           << "\" height=\""
+           << pixelSize
+           << "\" x=\""
+           << pixelSize*(j-1)
+           << "\" y=\""
+           << pixelSize*(i-1)
+           << "\" fill=\""
+           << getPixel(i, j)
+           << "\" />"
+           << std::endl;
+
+  file << "</svg>"
+       << std::endl;
+
+  file.close();
+}
+*/
