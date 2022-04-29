028f404d80e19c9c9fb596d661325eff817ec769
Alisa Hasanli <alisahesenli19@gmail.com>
2022-04-29 18:44:35 +0200

    Improvements

--- AIPetu/Image.cpp
+++ AIPetu/Image.cpp
@@ -12,10 +12,17 @@
 #include <stdexcept>
 #include "Image.h"
 #include "Color.h"
+#include <cmath>
 
 using namespace std;
 
-
+//----------------------------------------------Constructeur Image------------------------------------------
+/**
+ * @brief Construct a new Image:: Image object
+ * 
+ * @param width 
+ * @param height 
+ */
 Image::Image(int width, int height)
 {
      w = width;
@@ -27,51 +34,105 @@ Image::Image(int width, int height)
      }
 }
 
+//----------------------------------------------Destructeur Image------------------------------------------
+/**
+ * @brief Destroy the Image:: Image object
+ * 
+ */
 Image::~Image()
 {
      delete [] tabPixel;
 }
 
+//------------------------------------------------------Getters------------------------------------------
+/**
+ * @brief Returns width of this
+ * 
+ * @return int 
+ */
 int Image::width() const
 {
      return w;
 }
 
+/**
+ * @brief Returns height of this
+ * 
+ * @return int 
+ */
 int Image::height() const
 {
      return h;
 }
 
+/**
+ * @brief Returns size(number of pixels) of this
+ * 
+ * @return int 
+ */
 int Image::size() const
 {
      return h*w;
 }
 
+/**
+ * @brief Gets pixel in the i-th row and j-th column
+ * 
+ * @param i 
+ * @param j 
+ * @return Color 
+ */
 Color Image::getPixel(int i, int j) const
 {
      assert((1 <= i <= height()) && (1 <= j <= width()));
      return tabPixel[((i-1) * w + j)];
 }
 
+/**
+ * @brief sets the given color to the i-th row and j-th column
+ * 
+ * @param i 
+ * @param j 
+ * @param col 
+ */
 void Image::setPixel(int i, int j, Color col)
 {
      assert((1 <= i <= height()) && (1 <= j <= width()));
      tabPixel[((i-1) * w + j)] = col;
 }
 
+/**
+ * @brief Maps a coordinate (i, j) to a unique identifier
+ * 
+ * @param i 
+ * @param j 
+ * @return int 
+ */
 int Image::toIndex(int i, int j) const
 {
      return (i*w)+j;
 }
 
+/**
+ * @brief Maps an identifier to the corresponding coordinates
+ * 
+ * @param k 
+ * @return std::pair<int,int> 
+ */
 std::pair<int,int> Image::toCoordinate(int k) const
 {
      int i,j;
-     j = k % w;
-     i = (k-j) / w;
+     i = (k / this->width()) + 1;
+     j = k % this->width();
+     if (j == 0) { j = this->width(); }
      return std::make_pair(i,j);
 }
 
+/**
+ * @brief Fills the Image with the given color
+ * 
+ * @param c 
+ */
 void Image::fill(Color c)
 {
      for (int i = 0; i < size(); i++)
@@ -80,6 +141,15 @@ void Image::fill(Color c)
      }
 }
 
+/**
+ * @brief Fills a given square with given color
+ * 
+ * @param i1 
+ * @param j1 
+ * @param i2 
+ * @param j2 
+ * @param c 
+ */
 void Image::fillRectangle(int i1, int j1, int i2, int j2, Color c)
 {
 
@@ -90,6 +160,12 @@ void Image::fillRectangle(int i1, int j1, int i2, int j2, Color c)
      
 }
 
+/**
+ * @brief creates an SVG file
+ * 
+ * @param filename 
+ * @param pixelSize 
+ */
 void Image::writeSVG(const std::string& filename, int pixelSize) const
 {
   assert(pixelSize > 0);
@@ -129,6 +205,11 @@ void Image::writeSVG(const std::string& filename, int pixelSize) const
   file.close();
 }
 
+/**
+ * @brief creates an AIP file
+ * 
+ * @param filename 
+ */
 void Image::writeAIP(const std::string& filename) const
 {
      std::ofstream file;
@@ -152,6 +233,12 @@ void Image::writeAIP(const std::string& filename) const
      file.close();
 }
 
+/**
+ * @brief reads an AIP file and returns an Image
+ * 
+ * @param filename 
+ * @return Image 
+ */
 Image Image::readAIP(const std::string& filename)
 {
      std::ifstream file;
@@ -165,8 +252,8 @@ Image Image::readAIP(const std::string& filename)
      
      
 
-     file >> widthFILE;
-     file >> heightFILE;
+     file >> widthFILE; //Lire 1er ligne de la file jusqu'a un espace
+     file >> heightFILE; //Lire 1er ligne de la file jusqu'a un espace aprés file >> widthFILE
 
 
      Image image(std::stoi(widthFILE),std::stoi(heightFILE));
@@ -175,16 +262,18 @@ Image Image::readAIP(const std::string& filename)
 
      for (int i = 1; i <= std::stoi(heightFILE); i++)
      {
-          file >> currentLine;
+          file >> currentLine; //Gets ASCII characteres from file
           for (int j = 0; j < std::stoi(widthFILE); j++)
           {
-               int colorCode = (int)(currentLine[j]);
+               int colorCode = (int)(currentLine[j]); // Transform ASCII charracter to int
+
                if (colorCode == 52) { c = Color::Green; }
                if (colorCode == 51) { c = Color::Blue; }
                if (colorCode == 50) { c = Color::Red; }
                if (colorCode == 49) { c = Color::White; }
                if (colorCode == 48) { c = Color::Black; }
-               image.setPixel(i,j+1,c);
+
+               image.setPixel(i,j+1,c); //Sets the corresponding color to pixel
           }
 
      }
@@ -192,6 +281,14 @@ Image Image::readAIP(const std::string& filename)
      return image;
 }
 
+
+/**
+ * @brief Returns true if this and img are equal
+ * 
+ * @param img 
+ * @return true 
+ * @return false 
+ */
 bool Image::operator==(const Image& img) const
 {
      bool isEqual = false;
@@ -208,11 +305,28 @@ bool Image::operator==(const Image& img) const
      return isEqual;
 }
 
+/**
+ * @brief Returns true if this and img are different
+ * 
+ * @param img 
+ * @return true 
+ * @return false 
+ */
 bool Image::operator!=(const Image& img) const{
      !operator==(img);
      return true;
 }
 
+/**
+ * @brief Returns true if (i1, j1) and (i2, j2) are two consecutive piels of this
+ * 
+ * @param i1 
+ * @param j1 
+ * @param i2 
+ * @param j2 
+ * @return true 
+ * @return false 
+ */
 bool Image::areConsecutivePixels(int i1, int j1, int i2, int j2){
      assert(((1 <= i1 <= w ) && (1 <= j1 <= h )) && ((1 <= i2 <= w ) && (1 <= j2 <= h )));
      //Droite,bas,gauche,haut
@@ -225,6 +339,14 @@ bool Image::areConsecutivePixels(int i1, int j1, int i2, int j2){
      
 }
 
+/**
+ * @brief Tests if (i,j) is the coordinates of some pixel of this
+ * 
+ * @param i 
+ * @param j 
+ * @return true 
+ * @return false 
+ */
 bool Image::isValidCoordinate(int i, int j) const{
      if(1 <= i <= width() && 1 <= j <= height())
      {
@@ -235,9 +357,25 @@ bool Image::isValidCoordinate(int i, int j) const{
      
 }
 
-Image makeRandomImage(int w, int h){
+/**
+ * @brief creates an w x h Image with random colors 
+ * 
+ * @param w 
+ * @param h 
+ * @return Image 
+ */
+Image makeRandomImage(int w, int h)
+{
      Image img(w,h);
-     //La remplir d'une couleur random ?
+     for (int i = 1; i <= h; i++)
+     {
+          for (int j = 1; j <= w ; j++)
+          {
+               int colorCode = rand() % 4;
+               img.setPixel(i , j ,Color::makeColor(colorCode));
+          }
+     }
+     
      return img;
 }
 
--- AIPetu/main.cpp
+++ AIPetu/main.cpp
@@ -19,26 +19,28 @@ int main(void)
 {
   srand(time(nullptr));
 
+  // ------------------------------------------------------------Création Objet Image de 3x3 ----------------------------------------
+  Image img(3,3);
+  //-------------------------------------------------------------Test les methodes de Image.h-------------------------------------
+  
+  img.toCoordinate(7);
+
   Color col = Color::Red;
+  img.setPixel(3,2,col);
 
-  Image img(50,30);
+  img.fillRectangle(1,1,2,2,Color::Blue);
 
-  //img.setPixel(3,2,col);
+  //Test writeSVG et writeAIP
+  img.writeSVG("test",1);
+  img.writeAIP("test");
 
-  //img.fillRectangle(1,1,2,2,Color::Blue);
-
-  //img.writeSVG("test1",1);
-  //img.writeAIP("test1");
-
+  //Test de readAIP et puis writeSVG
   Image image(Image::readAIP("amazonie_0"));
+  image.writeSVG("amazonie0",50);
 
-  image.writeSVG("test2",3);
-  
-
-  //Image image(Image::readAIP("amazonie_0"));
-
-  //image.writeSVG("amazonie0",50);
-  
+  //Test pour makeRandomImage(w,h)
+  Image image = makeRandomImage(3,3);
+  image.writeSVG("randomImage",1);
 
   return 0;
 }
