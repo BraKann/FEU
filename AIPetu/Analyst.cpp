d7c3b89452f31f1c77c7db8cf8f31d0a943580e3
Alisa Hasanli <alisahesenli19@gmail.com>
2022-04-29 19:43:14 +0200

    Analyst

--- AIPetu/Analyst.cpp
+++ AIPetu/Analyst.cpp
@@ -8,27 +8,53 @@
 
 #include <cassert>
 #include "Analyst.h"
+#include "Image.h"
 
 using namespace std;
 
-Analyst::Analyst(const Image& img)
-{
-    Analyst *image = new Analyst(img);
-}
-
-Analyst::~Analyst()
-{
-    delete image;
-}
-
-bool Analyst::belongToTheSameZone(int i1, int j1, int i2, int j2)
-{
-    if (image->getPixel(i1,j1) == image->getPixel(i2,j2))
+/**
+ * @brief Construct a new Analyst:: Analyst object
+ * 
+ * @param img 
+ */
+Analyst::Analyst(const Image& img){
+    int numPart = 0;
+    Image image = img;
+    tabPart = new Partition[image.size()];
+    for(int i = 0; i < image.height(); i++)
     {
-        return true ; 
+        for(int j = 0; j < image.width(); j++)
+        {
+            //Cas particulier : Pixel haut gauche, pixels de la premiere ligne et premiere colonne
+            if( (1 <= i-1 <= image.width())  (1 <= j-1 <= image.height()) )
+            { 
+                //Si le pixel haut ou le pixel gauche precede le pixel actuel, ou car cas particulier
+                if( (image.areConsecutivePixels(i,j,i-1,j))  (image.areConsecutivePixels(i,j,i,j-1)) ) 
+                {
+                    //Si le pixel en haut ou le pixel a gauche du pixel courant sont de meme couleur
+                    if( (image.getPixel(i,j) == image.getPixel(i-1,j)) || (image.getPixel(i,j) == image.getPixel(i,j-1)) ) 
+                    {
+                        //Le pixel actuel prend le dernier numéro de partition attribuer 
+                        tabPart[i].nPartirion = numPart;
+                        tabPart[i].indexCouleur = image.getPixel(i,j);
+                    }
+                }
+            } else {
+                //attribue un nouveau numéro de partition pour le pixel actuel
+                tabPart[i].nPartirion = numPart++;
+                tabPart[i].indexCouleur = image.getPixel(i,j);
+            }
+        }
     }
 }
 
-
+/**
+ * @brief Destroy the Analyst:: Analyst object
+ * 
+ */
+Analyst::~Analyst()
+{
+    delete [] tabPart;
+}
 
 
